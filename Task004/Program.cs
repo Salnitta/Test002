5039f7962756731b7d770f379a3273cb81306357
Сальникова Татьяна Валерьевна <salnitta@MacBook-Air-Salnikova.local>
2022-06-06 21:39:53 +0300

    Task004

---
+++ Task004/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает 
+// одно число (N), а на выходе показывает все 
+// целые числа в промежутке от -N до N
+
+Console.WriteLine("Введите целое положительное число");
+int N = int.Parse(Console.ReadLine());
+int num = N * 2 + 1;
+int count = 1;
+while(num > count)
+{
+Console.Write(-N + ", ");
+count++;
+N--;
+}
+Console.Write(-N);
