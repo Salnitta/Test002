6249fe12d41b87b2dc58e101c6fb944967080722
Сальникова Татьяна Валерьевна <salnitta@MacBook-Air-Salnikova.local>
2022-06-06 22:08:26 +0300

    Task005

---
+++ Task005/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает 
+// последнюю цифру этого числа.
+
+Console.WriteLine("Введите трехзначное положительное число");
+int N = int.Parse(Console.ReadLine());
+int result = N % 100 % 10;
+if(N > 999)
+{
+    Console.WriteLine("Введено некорректное число");
+} 
+else if (N < 100)
+{
+    Console.WriteLine("Введено некорректное число");  
+}
+else Console.WriteLine(result);
