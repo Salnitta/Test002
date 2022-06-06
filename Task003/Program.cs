793c243d207097e62cbd6950b24fef26fa02fcc7
Сальникова Татьяна Валерьевна <salnitta@MacBook-Air-Salnikova.local>
2022-06-06 17:52:47 +0300

    changed

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -6,11 +6,10 @@ Console.WriteLine("Введите число от 1 до 7");
 int day = int.Parse(Console.ReadLine());
 
 if(day == 1) Console.WriteLine("Понедельник");
-if(day == 2) Console.WriteLine("Вторник");
-if(day == 3) Console.WriteLine("Среда");
-if(day == 4) Console.WriteLine("Четверг");
-if(day == 5) Console.WriteLine("Пятница");
-if(day == 6) Console.WriteLine("Суббота");
-if(day == 7) Console.WriteLine("Воскресенье");
-if(day < 1) Console.WriteLine("Введено недопустимое число");
-if(day > 7) Console.WriteLine("Введено недопустимое число");
+else if(day == 2) Console.WriteLine("Вторник");
+else if(day == 3) Console.WriteLine("Среда");
+else if(day == 4) Console.WriteLine("Четверг");
+else if(day == 5) Console.WriteLine("Пятница");
+else if(day == 6) Console.WriteLine("Суббота");
+else if(day == 7) Console.WriteLine("Воскресенье");
+else Console.WriteLine("Введено недопустимое число");
