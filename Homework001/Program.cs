eda12faa3d2357d870eb3ad43b38d48b25efe8e8
Сальникова Татьяна Валерьевна <salnitta@MacBook-Air-Salnikova.local>
2022-06-08 23:04:19 +0300

    Homework001

---
+++ Homework001/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает 
+// два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число:");
+double numberA = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+double numberB = double.Parse(Console.ReadLine());
+if(numberA > numberB)
+{
+    Console.WriteLine(numberA + "- большее число");
+    Console.WriteLine(numberB + "- меньшее число");
+}
+else if(numberA < numberB)
+{
+    Console.WriteLine(numberB + "- большее число");
+    Console.WriteLine(numberA + "- меньшее число");
+}
+else
+Console.WriteLine("Введенные числа равны");
