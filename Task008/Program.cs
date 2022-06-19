eefc1b23d16968912479161fb6b0e3d503e9865d
Сальникова Татьяна Валерьевна <salnitta@MacBook-Air-Salnikova.local>
2022-06-19 18:45:25 +0300

    Task008

---
+++ Task008/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает на вход 
+// число (А) и выдает сумму чисел от 1 до А.
+
+int Func(int num)
+{
+   int sum = 0;
+   for (int i = 1; i <= num; i++)
+   {
+        sum += i;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число:");
+int A = int.Parse(Console.ReadLine());
+
+int n = Func(A);
+Console.WriteLine(n);
+
+Console.WriteLine(Func(7));
+Console.WriteLine(Func(4));
+Console.WriteLine(Func(8));
+Console.WriteLine(Func(-4));
+Console.WriteLine(Func(0));
+Console.WriteLine(Func(1));
+
+// Console.WriteLine("Введите число:");
+// int A = int.Parse(Console.ReadLine());
+
+// int sum = 0;
+
+// for (int i = 1; i <= A; i++)
+// {
+//     sum += i;
+// }
+// Console.WriteLine(sum);
