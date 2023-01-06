360ab7ab207f12702006d545ae2f75d10502bb66
Salnitta <tatyana.saln@mail.ru>
2023-01-06 22:33:33 +0300

    Homework001v2.py

--- Python/HOMEWORKS/Homework001.py
+++ Python/HOMEWORKS/Homework001.py
@@ -86,11 +86,8 @@
 
 # ЗАДАЧА 2: Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-for i in range(2):
-    X = i
-    for j in range(2):
-        Y = j
-        for k in range(2):
-            Z = k
+for X in range(2):
+    for Y in range(2):
+        for Z in range(2):
             bool = not (X or Y or Z) == (not X and not Y and not Z)
             print(f'{X, Y, Z} - {bool}')
