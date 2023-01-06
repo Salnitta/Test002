884d16750f81dee5e187b9bd229b208b4f5310c2
Salnitta <tatyana.saln@mail.ru>
2023-01-06 11:08:48 +0300

    Discord003.py

--- Python/DISCORD/Discord003.py
+++ Python/DISCORD/Discord003.py
@@ -14,26 +14,25 @@ def decode(equation: dict) -> str:
     for key, value in equation.items():
         if value != 0:
             new_equation.append(f'{value}*x**{key}')
-        new_equation = ' + '.join(new_equation) + ' = 0'
-        new_equation = new_equation.replace('+ -', '- ')\
-            .replace('x**0', '').replace(' 1*x', ' x').replace('x**1', 'x')
-        if new_equation.startswith('1*x'):
-            return new_equation[2:]
-        elif new_equation.startswith('-1x'):
-            return new_equation.replace('-1*x', '-x')
-        else:
-            return new_equation
+    new_equation = ' + '.join(new_equation) + ' = 0'
+    new_equation = new_equation.replace('+ -', '- ')\
+            .replace('*x**0', '').replace(' 1*x', ' x').replace('x**1', 'x')
+    if new_equation.startswith('1*x'):
+        return new_equation[2:]
+    elif new_equation.startswith('-1x'):
+        return new_equation.replace('-1*x', '-x')
+    else:
+        return new_equation
 
-def encode(equation: str) -> dict:
-    equation = (' ' + equation.replace(' + ', '').replace(' - ', ' -')\
-        .replace('-x', '-1*x').replace(' x', ' 1*x')\
-        .replace('*x ', '*x**1 ').split()[:-2])
+def encode(equation: str):
+    equation = (' ' + equation).replace(' + ', ' ').replace(' - ', ' -')\
+        .replace('-x', '-1*x').replace(' x', ' 1*x').replace('*x ', '*x**1 ').split()[:-2]
     dict_equation = {}
-    for item in equation[1:]:
+    for item in equation:
         i = item.split('*x**')
         if len(i) > 1:
             dict_equation[int(i[1])] = int(i[0])
-        elif len(i) ==1 and i[0] != '':
+        elif len(i) == 1 and i[0] != '':
             dict_equation[0] = int(i[0])
     return dict_equation
 
@@ -46,21 +45,29 @@ def addition(first_eq: dict, second_eq: dict):
         final_eq[key] = first_eq.get(key, 0) + second_eq.get(key, 0)
     return final_eq
 
-
+# для одного уравнения:
 # equation = create_equation()
 # print(equation)
 # str_equation = decode(equation)
 # print(str_equation)
 # dict_equation = encode(str_equation)
 # print(dict_equation)
+# res_equation = decode(dict_equation)
+# print(res_equation)
 
+
+# для суммы двух уравнений:
 equation1 = create_equation()
 equation2 = create_equation()
-print(equation1)
-print(equation2)
-print(decode(equation1))
-print(decode(equation2))
-print(decode(addition(equation1, equation2)))
+str_eq1 = decode(equation1)
+str_eq2 = decode(equation2)
+print(str_eq1)
+print(str_eq2)
+dict_eq1 = encode(str_eq1)
+dict_eq2 = encode(str_eq2)
+dict_final = addition(dict_eq1, dict_eq2)
+str_final = decode(dict_final)
+print(str_final)
 
 
 
