9570e9cff86742ac8639fbea39307f2bed425ea9
Salnitta <tatyana.saln@mail.ru>
2023-01-08 00:04:50 +0300

    commenting

--- Python/pipPy/main.py
+++ Python/pipPy/main.py
@@ -1,19 +1,19 @@
 # Telegram bot
 
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-from bot_commands import*
+# from telegram import Update
+# from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+# from bot_commands import*
 
 
-app = ApplicationBuilder().token('5836719418:AAH6N-kDs8Lm17PkOu83gMNa9_HmrF5kA8w').build()
+# app = ApplicationBuilder().token('5836719418:AAH6N-kDs8Lm17PkOu83gMNa9_HmrF5kA8w').build()
 
-app.add_handler(CommandHandler("hi", hi_command))
-app.add_handler(CommandHandler("time", time_command))
-app.add_handler(CommandHandler("help", help_command))
-app.add_handler(CommandHandler("sum", sum_command))
+# app.add_handler(CommandHandler("hi", hi_command))
+# app.add_handler(CommandHandler("time", time_command))
+# app.add_handler(CommandHandler("help", help_command))
+# app.add_handler(CommandHandler("sum", sum_command))
 
-print('server start')
-app.run_polling()
+# print('server start')
+# app.run_polling()
 
 
 # Example matplotlib 2
